fac357916fea49314ba026a3ce31ecd04fc6ade8
Luisma <40072055+LuismadelValle@users.noreply.github.com>
2022-09-16 00:11:20 -0400

    First component created

--- plusstudio/src/App.vue
+++ plusstudio/src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import Footer from './components/Footer.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 });
 </script>
