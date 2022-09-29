476dab2c8cb0fd1d9b9623f762636ac58b067fde
Luisma <40072055+LuismadelValle@users.noreply.github.com>
2022-09-29 16:37:51 -0400

    adding table as component

---
+++ plusstudio/plusstudio/src/views/service.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar />
+    <ServTable />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Navbar from '@/components/Navbar.vue';
+import ServTable from '@/components/servicesTable.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    ServTable,
+    Footer
+  }
+}
+</script>
